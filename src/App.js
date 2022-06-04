77d0776081c9848dd42dfa6eca417c12ce537f5a
vadim <vadym.storozhuk@nure.ua>
2022-06-04 15:03:03 +0300

    uploaded proj

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import CComponent from './CComponent';
+import Fcomponent from './Fcomponent';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <header className='header'>
+        <img src = "logo192.png"></img>
+      </header>
+
+    <div className='nav'>
+      Navigation
+    </div>
+<div className = "middle"></div>
+<div>middle</div>
+
+    </div>
+  );
+}
+
+export default App;

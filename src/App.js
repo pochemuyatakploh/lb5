58f3606604ac8d9eb270336577eea5aabe43e4d4
vadim <vadym.storozhuk@nure.ua>
2022-06-04 16:06:29 +0300

    s Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Header from './components/Header';
 import Navigation from "./components/Navigation";
 import Middle from "./components/Middle";
 import Banner from "./components/Banner";
+import FooterCont from "./components/FooterCont";
+import UnderHeaderComp from "./components/UnderHeaderComp";
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header/>
+      <UnderHeaderComp/>
       <Navigation/>
       <Middle/>
       <Banner/>
-      
-    </div>
+      <FooterCont/>
+  </div>
   );
 }
 
---
+++ src/components/UnderHeaderComp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UnderHeader = () =>{
+    return (
+        <div className="under-header">
+            <h3>under header part</h3>
+        </div>
+    )
+}
+export default UnderHeader;
